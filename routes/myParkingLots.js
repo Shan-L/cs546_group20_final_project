9f470f622a85e53f1a3525a5b2f45df06d94a17d
Shan Leng <shanleng@shandembp-14.myfiosgateway.com>
2022-05-05 21:37:53 -0400

    Format code

--- routes/myParkingLots.js
+++ routes/myParkingLots.js
@@ -9,7 +9,7 @@ router.get('/:id', async (req, res) => {
     try {
         let myParkingLots = await parklotData.findAllParkingLotsByUploaderId(userId);
         let myParkingLotsExists = myParkingLots.length !== 0 ? true : false;
-        res.render('user/myParkingLots', { layout: 'user', title: 'Add New Parking Lot', userId: userId, myParkingLots: myParkingLots, myParkingLotsExists : myParkingLotsExists });
+        res.render('user/myParkingLots', { layout: 'user', title: 'Add New Parking Lot', userId: userId, myParkingLots: myParkingLots, myParkingLotsExists: myParkingLotsExists });
     } catch (e) {
         res.status(500).json({ error: e });
     }
